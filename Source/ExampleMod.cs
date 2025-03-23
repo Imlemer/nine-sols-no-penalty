5e3e0f19ea1120126400e3991e66156b7315b88f
kari <155239026+Imlemer@users.noreply.github.com>
2025-03-23 15:55:25 +0400

    Update ExampleMod.cs

--- Source/ExampleMod.cs
+++ Source/ExampleMod.cs
@@ -1,8 +1,7 @@
-﻿using BepInEx;
-using BepInEx.Configuration;
+using System.Threading.Tasks;
+using BepInEx;
 using HarmonyLib;
 using NineSolsAPI;
-using UnityEngine;
 
 namespace ExampleMod;
 
@@ -11,14 +10,36 @@ namespace ExampleMod;
 public class ExampleMod : BaseUnityPlugin {
 
     private Harmony harmony = null!;
-
-    private void Awake() {
+    private async Task Awake() {
         
         Log.Init(Logger);
         RCGLifeCycle.DontDestroyForever(gameObject);
 
         harmony = Harmony.CreateAndPatchAll(typeof(ExampleMod).Assembly);
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        while (Player.i == null) {
+            await Task.Run(async () => { await Task.Delay(1000); });
+            Log.Message("Waiting for player");
+        }
+
+        Log.Message("Player loaded");
+        
+        var playerLastBag = AccessTools.FieldRefAccess<PlayerDeadRecord, DropPickable>("lastDroppedBag");
+        int iteration = 0;
+
+        while (playerLastBag.Invoke(GameCore.Instance.deadRecord) == null) {
+            if (iteration == 10) {break;}
+            await Task.Run(async () => { await Task.Delay(1000); });
+            Log.Message("Waiting for bag");
+            iteration++;
+        }
+        
+        if (iteration != 10) {
+            var lastBag = playerLastBag.Invoke(GameCore.Instance.deadRecord);
+            Destroy(lastBag.gameObject); 
+            Log.Message("Last bag destroyed");
+        }
 
     }
 
