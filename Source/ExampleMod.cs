3bfb8e07e7f1b4c8af2b9a9557470386fec07b37
Imlemer <timurmagerramov616@gmail.com>
2025-03-23 15:02:55 +0400

    yes

---
+++ Source/ExampleMod.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using NineSolsAPI;
+using UnityEngine;
+
+namespace ExampleMod;
+
+[BepInDependency(NineSolsAPICore.PluginGUID)]
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class ExampleMod : BaseUnityPlugin {
+
+    private Harmony harmony = null!;
+
+    private void Awake() {
+        
+        Log.Init(Logger);
+        RCGLifeCycle.DontDestroyForever(gameObject);
+
+        harmony = Harmony.CreateAndPatchAll(typeof(ExampleMod).Assembly);
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+    }
+
+
+    private void OnDestroy() {
+
+        harmony.UnpatchSelf();
+
+    }
+}
